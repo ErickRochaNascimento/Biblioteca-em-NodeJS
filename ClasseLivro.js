7d2fcf18b783a3dac69777848982c01536a3792b
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2026-03-01 17:51:03 -0300

    adiciona classe Livro para representar livros na biblioteca

---
+++ ClasseLivro.js
@@ -0,0 +1,11 @@
+export class Livro {
+    constructor(titulo, autor, isbn, anoPublicacao, disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao
+        this.disponivel = disponivel;
+    
+    }
+
+}
